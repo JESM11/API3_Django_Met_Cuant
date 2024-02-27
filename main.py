588c2f94cb9ab0c4c4e2ac0a32bdf0ee85a337f0
abudi53 <abdallahffh@gmail.com>
2024-02-27 13:42:37 -0400

    arreglar segundo ejercicio

--- main.py
+++ main.py
@@ -10,27 +10,44 @@ def fibonacci(n) -> list:
         fib_sequence.append(fib_sequence[i-1] + fib_sequence[i-2])
     return jsonify(fib_sequence[:num]),200
 
-@app.route("/LCS/<strings>")
-def length_of_longest_common_substring(strings) -> int:
-    cadena = strings
-    separadas = cadena.split("y")
-    s1=separadas[0]
-    s2=separadas[1]
+@app.route("/LCS/<string1>/<string2>")
+def length_of_longest_common_substring(string1, string2) -> int:
+    m = len(string1)
+    n = len(string2)
+    dp = [[-1 for i in range(n + 1)]for j in range(m + 1)]
 
-    # Inicializar la matriz bidimensional
-
-    dp = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
-
-    # Iterar sobre las dos cadenas
-    for i in range(1, len(s1) + 1):
-        for j in range(1, len(s2) + 1):
-            if s1[i-1] == s2[j-1]:
-                dp[i][j] = dp[i-1][j-1] + 1
-            else:
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-
+    
     # Devolver la longitud de la subsecuencia común más larga
-    return jsonify(dp[-1][-1]),200
+    return jsonify(lcs(string1, string2, m, n, dp)),200
+
+def lcs(X, Y, m, n, dp):
+    """
+    Parametros:
+        X (str): Primera string.
+        Y (str): Segunda string string.
+        m, n: length de cada string.
+        dp (list): Lista 2D para usar como memoizacion.
+        
+    Retorna:
+        int: Length de la subsequencia mas larga.
+    """
+    if (m == 0 or n == 0):
+        return 0
+
+    # Si el valor ya fue calculado, retornar el valor.
+    if (dp[m][n] != -1):
+        return dp[m][n]
+    
+    # Si el ultimo caracter de ambas strings es igual, agregar 1 al resultado y llamar recursivamente con las strings sin el ultimo caracter.
+    if X[m - 1] == Y[n - 1]:
+        dp[m][n] = 1 + lcs(X, Y, m - 1, n - 1, dp)
+        return dp[m][n]
+    
+    # Si el ultimo caracter de ambas strings es diferente, retornar el maximo de llamar recursivamente con las strings sin el ultimo caracter de X y Y.
+    dp[m][n] = max(lcs(X, Y, m, n - 1, dp), lcs(X, Y, m - 1, n, dp))
+    return dp[m][n]
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
